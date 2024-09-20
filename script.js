ae68e0d0790ae9fb6f769795ad31a2be22d9bb24
Dudeyay <xuzhexihk@gmail.com>
2024-09-21 00:46:08 +0800

    Put the code to generate grid into a function. Made it so that before generating the grid it removes all elements in the container to reset. Added function to change background of boxes if hovered over and function to prompt user for input to change grid

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-let grid = document.createElement("div");
-const CONTAINER_WIDTH = 960;
-let gridNumber = 16;
-let dimension = String(Math.floor(CONTAINER_WIDTH/gridNumber));
-grid.style.width = dimension + "px";
-grid.style.height = dimension + "px";
-grid.style.border = "2px Black Solid";
-grid.style.boxSizing = "border-box";
-let container = document.querySelector(".container");
-for (i=0; i<Math.pow(gridNumber,2);i++) {
-    const gridClone = grid.cloneNode(true);
-    container.appendChild(gridClone);
+function changeBackground() {
+    this.style.backgroundColor = "Black";
 }
+
+function changeGridBasedOnInput(){
+    let gridNumber = parseInt(prompt("Enter number of squares per side"));
+    while (true) {
+        if (gridNumber > 100 || gridNumber < 1) {
+            gridNumber = parseInt(prompt("Please Enter a Number between 1-100"));
+        } else {
+            break;
+        }
+    }
+    generateGrid(gridNumber);
+}
+
+function generateGrid(gridNumber) {
+    let container = document.querySelector(".container");
+    while(container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+    let grid = document.createElement("div");
+    const CONTAINER_WIDTH = 960;
+    let dimension = String(Math.floor(CONTAINER_WIDTH / gridNumber));
+    grid.style.width = dimension + "px";
+    grid.style.height = dimension + "px";
+    grid.style.border = "2px Black Solid";
+    grid.style.boxSizing = "border-box";
+    for (i = 0; i < Math.pow(gridNumber, 2); i++) {
+        const gridClone = grid.cloneNode(true);
+        gridClone.addEventListener("mouseenter", changeBackground);
+        container.appendChild(gridClone);
+    }
+}
+
+generateGrid(16);
+btn = document.querySelector("button");
+btn.addEventListener("click", changeGridBasedOnInput);
+
+
 
 
 
